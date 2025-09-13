f7186805cfcbebf1b995fc19c4b07048d2241a70
Kiriewka <chebotar.kiryusha@gmail.com>
2025-09-13 03:27:19 +0300

    feat(profile): add orders history page and order details view

--- src/store/thunks/loyaltyThunk.ts
+++ src/store/thunks/loyaltyThunk.ts
@@ -1,8 +1,9 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
 import { instance } from "@/api/axios.api";
+import type { LoyaltyHistoryResponse, LoyaltyBonusResponse } from "@/types/Loyalty";
 
-export const loyaltyHistory = createAsyncThunk<any, void, { rejectValue: string }>(
+export const loyaltyHistory = createAsyncThunk<LoyaltyHistoryResponse, void, { rejectValue: string }>(
     "loyalty/loayaltyHistory",
     async (_, { rejectWithValue }) => {
         try {
@@ -14,7 +15,7 @@ export const loyaltyHistory = createAsyncThunk<any, void, { rejectValue: string 
     }
 );
 
-export const loyaltyBonus = createAsyncThunk<any, void, { rejectValue: string }>(
+export const loyaltyBonus = createAsyncThunk<LoyaltyBonusResponse, void, { rejectValue: string }>(
     "loyalty/loyaltyBonus",
     async (_, { rejectWithValue }) => {
         try {
