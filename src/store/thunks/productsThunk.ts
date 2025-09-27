6e0b163a09678d71a79ea8e6f5bd8c05b4d28ed3
Artem <artem@ryzhenko.com>
2025-09-27 23:12:55 +0300

    fetchProductsCounts merge conflict solution

--- src/store/thunks/productsThunk.ts
+++ src/store/thunks/productsThunk.ts
@@ -137,25 +137,6 @@ export const postProductSubscribe = createAsyncThunk<
     }
 );
 
-export const fetchProductActions = createAsyncThunk<
-    Product[],
-    { action: string },
-    { rejectValue: string }
->(
-    "products/fetchProductActions",
-    async ({ action }, { rejectWithValue }) => {
-        try {
-            const response = await axios.get<Product[]>(`${apiProductsUrl}/action/${action}`, {
-                params: { action },
-            });
-            return response.data;
-        } catch (error) {
-            const message = error instanceof Error ? error.message : "Unknown error";
-            return rejectWithValue(message);
-        }
-    }
-);
-
 export const fetchProductsCounts = createAsyncThunk<
     ProductsFilterCounts,
     { category?: string } | void,
@@ -177,6 +158,25 @@ export const fetchProductsCounts = createAsyncThunk<
     }
 );
 
+export const fetchProductActions = createAsyncThunk<
+    Product[],
+    { action: string },
+    { rejectValue: string }
+>(
+    "products/fetchProductActions",
+    async ({ action }, { rejectWithValue }) => {
+        try {
+            const response = await axios.get<Product[]>(`${apiProductsUrl}/action/${action}`, {
+                params: { action },
+            });
+
+            return response.data;
+        } catch (error) {
+            const message = error instanceof Error ? error.message : "Unknown error";
+            return rejectWithValue(message);
+        }
+    }
+);
 
 export const fetchSearchResults = createAsyncThunk<
     PaginatedProductsResponse,
