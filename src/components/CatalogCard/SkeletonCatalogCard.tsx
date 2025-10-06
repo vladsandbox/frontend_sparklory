d8135974c520523775c1cbd7b748473a91ca15fb
Artem <artem@ryzhenko.com>
2025-10-06 20:46:40 +0300

    Skeleton for the Catalog elements

---
+++ src/components/CatalogCard/SkeletonCatalogCard.tsx
@@ -0,0 +1,8 @@
+export default function SkeletonCatalogCard() {
+    return (
+        <div className="skeleton-catalog-card catalog-card">
+            <div className="skeleton-image shimmer" />
+            <div className="skeleton-line short shimmer" />
+        </div>
+    );
+}
